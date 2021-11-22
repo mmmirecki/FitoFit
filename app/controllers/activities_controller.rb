6b4e38c25071844ef8f95b5d9d7c614652459439
mmmirecki <mirecki.michal@gmail.com>
2021-11-22 17:28:51 +0100

    weekly distance + model validation

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,25 +1,31 @@
 class ActivitiesController < ApplicationController
-    def index
-        @activities = Activity.all
+  def index
+    @activities = Activity.all
+    @last_week_activities = 0
+    @activities.each do |act|
+      if (act[:created_at] + 7.days).strftime('%d/%m/%y') >= Time.now.strftime('%d/%m/%y') && (act[:distance])
+        @last_week_activities += act[:distance]
+      end
     end
-    
-    def new
-        @activity = Activity.new
-    end
+  end
 
-    def create
-        logger.debug(params[:activity][:start])
-        @activity_params = params[:activity]
-        @start = Start.create(address: @activity_params[:start])
-        @dest = Destination.create(address:@activity_params[@destination])
-        @activity = Activity.new
-        if @activity.save
-            @activity.start = @start
-            @activity.destination = @dest
-            redirect_to activities_path
-        else
-            render "new"
-        end
-        
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    logger.debug(params[:activity][:start])
+    @activity_params = params[:activity]
+    @activity = Activity.new(first_address: @activity_params[:start], second_address: @activity_params[:destination])
+    if @activity.save
+      @activity.start = Start.new(address: @activity_params[:start])
+      @activity.destination = Destination.new(address: @activity_params[:destination])
+      @activity[:distance] = @activity.start.distance_to(@activity.destination).round(2)
+      @activity.save
+      redirect_to activities_path
+    else
+      flash.now[:alert] = 'Please fill the form with correct data'
+      render 'new'
     end
+  end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,7 @@
 class Activity < ApplicationRecord
-    has_one :start, dependent: :destroy
-    has_one :destination, dependent: :destroy
+  validates :first_address, presence: true
+  validates :second_address, presence: true
+  validates :first_address, format: { with: /(.*,){2}.*/ }
+  has_one :start, dependent: :destroy
+  has_one :destination, dependent: :destroy
 end
- 
